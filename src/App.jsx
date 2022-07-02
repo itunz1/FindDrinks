543d82398de88bbcdfcbad659a56c2eceee8598f
Jose Tommaso <tommaso_j@hotmail.com>
2022-07-02 20:12:16 +0100

    drinks provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import { Container } from "react-bootstrap";
 import Form from "./components/Forms";
 import { CategoriesProvider } from "./context/CategoriesProvider";
+import { DrinksProvider } from "./context/DrinksProvider";
 
 function App() {
   return (
     <div className="App">
       <CategoriesProvider>
-        <header className="py-5">
-          <h1>Find the best drinks here!</h1>
-        </header>
+        <DrinksProvider>
+          <header className="py-5">
+            <h1>Find the best drinks here!</h1>
+          </header>
 
-        <Container className="mt-5">
-          <Form />
-        </Container>
+          <Container className="mt-5">
+            <Form />
+          </Container>
+        </DrinksProvider>
       </CategoriesProvider>
     </div>
   );
