ee3d9ae6291561e0c9df98c9d825e64d3c524c29
Jose Tommaso <tommaso_j@hotmail.com>
2022-07-02 23:59:09 +0100

    drink card

--- src/components/Drink.jsx
+++ src/components/Drink.jsx
@@ -1,5 +1,23 @@
-const Drink = () => {
-  return <div>Drink</div>;
+import { Col, Card, Button } from "react-bootstrap";
+
+const Drink = ({ drink }) => {
+  return (
+    <Col md={6} lg={3}>
+      <Card className="mb-4">
+        <Card.Img
+          src={drink.strDrinkThumb}
+          variant="top"
+          alt={`${drink.strDrink} drink`}
+        />
+        <Card.Body>
+          <Card.Title>{drink.strDrink}</Card.Title>
+          <Button variant="warning" className="w-100 text-uppercase mt-2">
+            Show drink
+          </Button>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
 };
 
 export default Drink;
