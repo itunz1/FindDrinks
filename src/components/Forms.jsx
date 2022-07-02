7672fea35ee68cea9187382de1aaa37c95836b76
Jose Tommaso <tommaso_j@hotmail.com>
2022-07-02 20:03:48 +0100

    form states

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -1,8 +1,33 @@
-import { Button, Form, Row, Col } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Form, Row, Col, Alert } from "react-bootstrap";
+import useCategories from "../hooks/useCategories";
 
 const Forms = () => {
+  const [search, setSearch] = useState({
+    drinkName: "",
+    category: "",
+  });
+  const [alert, setAlert] = useState("");
+  const { categories } = useCategories();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(Object.values(search).includes(""));
+    if (Object.values(search).includes("")) {
+      setAlert("Please fill in all fields");
+      return;
+    }
+    setAlert("");
+  };
+
   return (
     <Form>
+      {alert && (
+        <Alert variant="danger" className="text-center">
+          {alert}
+        </Alert>
+      )}
+
       <Row>
         <Col md={6}>
           <Form.Group className="mb-3">
@@ -12,14 +37,44 @@ const Forms = () => {
               type="text"
               placeholder="Enter drink name"
               name="drinkName"
+              value={search.drinkName}
+              onChange={(e) =>
+                setSearch({ ...search, [e.target.name]: e.target.value })
+              }
             />
           </Form.Group>
         </Col>
         <Col md={6}>
           <Form.Label htmlFor="category">Category</Form.Label>
-          <Form.Select id="category" name="category">
-            <option>Select a drink</option>
+          <Form.Select
+            id="category"
+            name="category"
+            onChange={(e) =>
+              setSearch({ ...search, [e.target.name]: e.target.value })
+            }
+          >
+            <option value="">Select a drink</option>
+            {categories.map((category) => {
+              return (
+                <option key={category.strCategory} value={category.strCategory}>
+                  {category.strCategory}
+                </option>
+              );
+            })}
           </Form.Select>
+        </Col>
+      </Row>
+
+      <Row className="justify-content-end">
+        <Col md={3}>
+          <Button
+            variant="danger"
+            className="text-uppercase w-100 mt-4 mt-md-0"
+            onClick={handleSubmit}
+            type="submit"
+          >
+            Search
+          </Button>
         </Col>
       </Row>
     </Form>
